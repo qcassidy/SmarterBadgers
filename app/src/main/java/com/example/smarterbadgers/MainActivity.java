5b26c6dce5dc3368d8d2ffb296dfec600dbea25b
Quinn Cassidy <qcassidy@wisc.edu>
2021-10-29 12:34:21 -0500

    menu and empty activity for map

--- app/src/main/java/com/example/smarterbadgers/MainActivity.java
+++ app/src/main/java/com/example/smarterbadgers/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.smarterbadgers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,4 +19,10 @@ public class MainActivity extends AppCompatActivity {
 //        Intent intent = new Intent(this, TimerActivity.class);
 //        startActivity(intent);
     }
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            return false;
+        }
+    };
 }
