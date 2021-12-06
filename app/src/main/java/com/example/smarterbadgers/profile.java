416c1e888ed3c1d30d17361fcb8d8b20d5365eac
kevin <kfang22@wisc.edu>
2021-12-05 21:39:14 -0600

    Profile GUI Added

--- app/src/main/java/com/example/smarterbadgers/profile.java
+++ app/src/main/java/com/example/smarterbadgers/profile.java
@@ -3,12 +3,31 @@ package com.example.smarterbadgers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class profile extends AppCompatActivity {
 
+    private ImageView achieve;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_main);
+
+        achieve = findViewById(R.id.imageView8);
+        achieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openDialog();
+            }
+        });
+
+
+    }
+    public void openDialog(){
+        AchievementDialog exampleDialog = new AchievementDialog();
+
+
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 }
