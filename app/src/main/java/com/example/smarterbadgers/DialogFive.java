f394bb6f80cdf2b92006ec62d21db12753fdb962
kevin <kfang22@wisc.edu>
2021-12-12 19:40:07 -0600

    Profile finished

--- app/src/main/java/com/example/smarterbadgers/DialogFive.java
+++ app/src/main/java/com/example/smarterbadgers/DialogFive.java
@@ -2,25 +2,49 @@ package com.example.smarterbadgers;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class DialogFive extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("This is dialog 5")
-                .setMessage("This is a test \n This is a test 5")
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("com.example.smarterbadgers", Context.MODE_PRIVATE);
+        int numAssign = sharedPreferences.getInt("completed_assignment_preference_key", 0);
 
-                    }
-                });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        if(numAssign >= 10) {
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar c = Calendar.getInstance();
+            String date = sdf.format(c.getTime());
+            builder.setTitle("Finished 10 Assignment!")
+                    .setMessage("Date Obtained:\n " +  date + "\n\nCongratulations!!!")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+        }else{
+            builder.setTitle("Finished 10 Assignment!")
+                    .setMessage("Date Obtained: not yet obtained")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+        }
         return builder.create();
     }
 }
--- app/src/main/java/com/example/smarterbadgers/DialogTwo.java
+++ app/src/main/java/com/example/smarterbadgers/DialogTwo.java
@@ -2,25 +2,48 @@ package com.example.smarterbadgers;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 
 public class DialogTwo extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("This is dialog 2")
-                .setMessage("This is a test")
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("com.example.smarterbadgers", Context.MODE_PRIVATE);
+        int recordedTime = sharedPreferences.getInt("timestudied", 0);
 
-                    }
-                });
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        if(recordedTime >= 120) {
+
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Calendar c = Calendar.getInstance();
+            String date = sdf.format(c.getTime());
+            builder.setTitle("Study 2 hours!")
+                    .setMessage("Date Obtained: " +  date + "\n\nCongratulations!!!")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+        }else{
+            builder.setTitle("Study 2 hours!")
+                    .setMessage("Date Obtained: not yet obtained")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+        }
         return builder.create();
     }
 }
